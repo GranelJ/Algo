545003776418f68eb45a2d1b3aed02bed5364b26
GranelJ <joris.granel@etu.umontpellier.fr>
2016-11-29 12:24:56 +0100

    modif
    Test ajouter

--- Test 2.0.py
+++ Test 2.0.py
@@ -5,5 +5,44 @@ import TypeJoueur
 import TypeFlotte
 import TypeGrille
 
-def testfp1()
+def test_bateau():
+	bateau = TypeBateau.Bateau(11,10,3,1,0)
+	assert TypeBateau.NumeroBateau(bateau) == 1
+	assert TypeBateau.TailleBateau(bateau) == 3
+	assert TypeBateau.DirectionBateau(bateau) == 0
+	assert TypeBateau.LigneBateau(bateau) == 10
+	assert TypeBateau.ColonneBateau(bateau) == 11
+	return True
 #teste la propriété 1 de la fonction f 
+
+
+def test_Joueur():
+	bateau1 = TypeBateau.Bateau(1,5,1,1,0)
+	bateau2 = TypeBateau.Bateau(3,10,2,2,0)
+	bateau3 = TypeBateau.Bateau(10,15,3,3,1)
+	bateau4 = TypeBateau.Bateau(15,17,3,4,0)
+	bateau5 = TypeBateau.Bateau(8,9,4,5,1)
+	grille = TypeGrille.Grille(21,21)
+	flotte = TypeFlotte.Flotte(bateau1,bateau2,bateau3,bateau4,bateau5)
+	joueur = TypeJoueur.Joueur(flotte,grille)
+	assert TypeJoueur.Flottejoueur() == flotte
+	assert TypeJoueur.Grillejoueur() == grille
+	return True
+
+def test_Grille():
+	grille = TypeGrille.Grille(21,21)
+
+
+
+def test_Flotte():
+	bateau1 = TypeBateau.Bateau(1,5,1,1,0)
+	bateau2 = TypeBateau.Bateau(3,10,2,2,0)
+	bateau3 = TypeBateau.Bateau(10,15,3,3,1)
+	bateau4 = TypeBateau.Bateau(15,17,3,4,0)
+	bateau5 = TypeBateau.Bateau(8,9,4,5,1)
+	grille = TypeGrille.Grille(21,21)
+	flotte = TypeFlotte.Flotte(bateau1,bateau2,bateau3,bateau4,bateau5)
+	assert TypeFlotte.BateauxRestants() == 5
+	grille = TypeGrille.ModifVal(0,1,5)
+	assert TypeFlotte.Coule(1,grille) == True
+	assert TypeFlotte.BateauxRestants() == 4
